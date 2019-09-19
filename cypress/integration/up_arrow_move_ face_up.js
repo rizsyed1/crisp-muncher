05157c59283b595f0cf451988d3ce9392cb0a8b8
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-09-19 22:38:27 +0100

    removed cypress test on canvas element

--- cypress/integration/up_arrow_move_ face_up.js
+++
@@ -1,9 +0,0 @@
-describe('The up arrow key', () => {
-    it('moves the face up', () => {
-        cy.get('body')
-        .trigger('keydown', {keycode: 38})
-        .trigger('keydown', {keycode: 38})
-        .trigger('keydown', {keycode: 38})
-        .trigger('keydown', {keycode: 38})
-    })
-})
